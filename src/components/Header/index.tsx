bdd6d85f865aa41c8367629c5d9d66d46843bed7
jainaldo <jainaldosilva@gmail.com>
2023-04-14 15:01:04 -0300

    Componente: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <div className={`${commonStyles.container} ${styles.content}`}>
+        <img src="/images/Logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
 }
